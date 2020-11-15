5494ebfa975274e52c386f4b8663e9df429d4be5
Abimbola-ai <60826354+Abimbola-ai@users.noreply.github.com>
2020-11-15 19:58:37 +0100

    updated notebook

--- new.py
+++ new.py
@@ -20,8 +20,8 @@ df2 = pd.DataFrame({"Wellhead Temp. (C)":np.random.uniform(41.07, 73.87,10000),
                     "BWPD (barrel of water produced per day)":np.random.uniform(40.61, 9314.26, 10000),
                     "BSW - basic solid and water (%)":np.random.uniform(0.13, 89.26, 10000),
                     "CO2 mol. (%) @ 25 C & 1 Atm.":np.random.uniform(0.6786, 4.2983, 10000),
-                    "Gas Grav.":np.random.uniform(0.7111, 0.9319, 10000)}) 
+                    "Gas Grav.":np.random.uniform(0.7111, 0.9319, 10000)}).round(4)
 
-df2.to_csv('fake_data2.csv')
-# df2 = pd.read_csv('fake_data.csv', sep=',')
-# print(df2.describe().T)
+df2.to_csv('fake_data2.csv', index = False)
+df2 = pd.read_csv('fake_data2.csv', sep=',')
+print(df2.describe().T)
